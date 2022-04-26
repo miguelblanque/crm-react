901dc33b6aec8de6c88ee24dc4e9365415916b45
miguelblanque <miguelblanque@gmail.com>
2022-04-26 19:53:11 +0200

    Consultar API para obtener Clientes

--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import {useEffect , useState} from 'react'
 
 const Inicio = () => {
+
+  const [clientes, setClientes] = useState([])
+
+  useEffect(()=>{
+      const orbtenerClientesAPI = async () => {
+        try{
+           const url = 'http://localhost:4000/clientes'
+           const respuesta = await fetch(url)
+           const resultado = await respuesta.json()
+
+           console.log(resultado)
+
+        }catch (error){
+          console.log (error)
+        }
+      }
+  },[])
+
   return (
     <div>
         <h1>Desde Inicio</h1>
